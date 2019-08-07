1759f233539d27ab72cf96402147cf28b2ca7ba7
rreenn <1163393838@qq.com>
2019-08-07 23:02:34 +0800

    feat: profile

--- src/pages/profile/index.tsx
+++ src/pages/profile/index.tsx
@@ -1,7 +1,22 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import './index.scss'
+import { getTopicList } from '@/api';
 
 const Profile = () => {
+  const [topicList, setTopicList] = useState([])
+  useEffect(() => {
+    const payload = {
+      page: 1,
+      tab: 'all',
+      limit: 40
+    }
+    getTopicList(payload).then((res: any) => {
+      if (res.success) {
+        const noReplyTopic = res.data.filter(item => item.reply_count === 0)
+        setTopicList(noReplyTopic.slice(0, 5))
+      }
+    })
+  }, [])
 
   const renderLogin = () => {
     return (
@@ -16,9 +31,45 @@ const Profile = () => {
   }
 
   const renderReply = () => {
+    const replyList = topicList.map((item: any) => {
+      return (
+        <span className="no-reply-topic" key={item.id}>{item.title}</span>
+      )
+    })
     return (
-      <div>
+      <div className="no-reply">
+        <span className="no-reply-title">无人回复的话题</span>
+        {replyList}
+      </div>
+    )
+  }
 
+  const renderTop100 = () => {
+    return (
+      <div className="top100">
+        <span className="top100-title">积分榜 TOP 100 &gt;&gt;</span>
+        <span className="top100-user">21555 i5ting</span>
+        <span className="top100-user">15545 alsotang</span>
+        <span className="top100-user">9350 leapon</span>
+        <span className="top100-user">8835 atian25</span>
+        <span className="top100-user">8780 jiyinyiyong</span>
+        <span className="top100-user">7245 yakczh</span>
+        <span className="top100-user">6780 im-here</span>
+        <span className="top100-user">6075 DevinXian</span>
+        <span className="top100-user">5815 chapgaga</span>
+        <span className="top100-user">5350 magicdawn</span>
+      </div>
+    )
+  }
+
+  const renderFriendShip = () => {
+    return (
+      <div className="frend-ship">
+        <span className="frend-ship-title">友情社区</span>
+        <img className="frend-ship-image" src="http://static2.cnodejs.org/public/images/ruby-china-20150529.png" alt="" />
+        <img className="frend-ship-image" src="//static2.cnodejs.org/public/images/golangtc-logo.png" alt="" />
+        <img className="frend-ship-image" src="//static2.cnodejs.org/public/images/phphub-logo.png" alt="" />
+        <img className="frend-ship-image" src="//static.cnodejs.org/FjLUc7IJ2--DqS706etPQ1EGajxK" alt="" />
       </div>
     )
   }
@@ -27,6 +78,8 @@ const Profile = () => {
     <div className="profile">
       {renderLogin()}
       {renderReply()}
+      {renderTop100()}
+      {renderFriendShip()}
     </div>
   )
 }
