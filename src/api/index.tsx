48643f219c808b6efb49e3ae844c3e7c314e5076
rreenn <1163393838@qq.com>
2019-08-30 09:01:59 +0800

    feat: topics markdown

--- src/api/index.tsx
+++ src/api/index.tsx
@@ -2,4 +2,7 @@ import http from '@/server'
 
 export const getTopicList = (params) => http.get('/topics', { params })
 
-export const getTopicContent = (params) => http.get(`/topics/${params.topicId}`)
+export const getTopicContent = (params) => http.get(`/topic/${params.topicId}`)
+
+// 获取主体详情
+// export const getT
--- src/pages/topics/index.tsx
+++ src/pages/topics/index.tsx
@@ -1,10 +1,26 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import QueryString from 'query-string'
+import marked from 'marked'
+import { getTopicContent } from '@/api'
 
-const Home = () => {
+const Topics = () => {
+  const [content, setContent] = useState('')
+  useEffect(() => {
+    const { id } = QueryString.parse(window.location.search)
+    const payload = {
+      topicId: id
+    }
+    getTopicContent(payload).then((res: any) => {
+      if (res.success) {
+        setContent(res.data.content)
+      }
+    })
+  }, [])
+  function createMarkup() {
+    return { __html: marked(content) };
+  }
   return (
-    <div>
-      haha
-    </div>
+    <div dangerouslySetInnerHTML={createMarkup()} />
   )
 }
-export default Home
+export default Topics
